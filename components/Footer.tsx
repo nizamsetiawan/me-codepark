7f02f2d88324e3a11483cde52680cdc8e6c0dc12
M. Nizam Setiawan <nizamsetiawan15@gmail.com>
2024-07-28 01:58:40 +0700

    update : footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,11 +5,7 @@ export default function Footer() {
     <section>
       <div className="flex flex-col gap-4 lg:px-6 mt-16">
         <p className="text-sm text-start text-muted-foreground">
-          Based on{" "}
-          <a className="text-foreground" href="https://brittanychiang.com">
-            Brittany Chiang&#39;s website
-          </a>{" "}
-          (they awesome). Coded in{" "}
+          Coded in{" "}
           <a className="text-foreground" href="https://code.visualstudio.com/">
             Visual Studio Code.
           </a>{" "}
